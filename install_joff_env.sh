0e8cfffa17a8842bdf2a77deaeb2522426990510
Joffrey Lagut <j.lagut@live.fr>
2023-05-05 19:15:40 +0200

    Update install script to install brew, nvim, ripgrep and setup packer

--- install_joff_env.sh
+++ install_joff_env.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
+# Save the files that will be erased if they exists
+mkdir -p .config-backup
+mv .config/nvim .config-backup
+mv .tmux.conf .config-backup
+
 # Clone the bare repository from Github, clean local config and set alias
 # NOTE: thanks to https://www.atlassian.com/git/tutorials/dotfiles
 git clone --bare https://github.com/JoffreyLGT/dotfiles.git $HOME/.cfg
-# Set config alias into .bashrc
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' >> $HOME/.bashrc
-source $HOME/.bashrc
 
-#function config {
-#   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
-#}
-mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
-config checkout
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+config checkout --force
 config config status.showUntrackedFiles no
+
+# Set config alias into .bashrc
+echo 'alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' >> $HOME/Workspace/bash/testfile
+
+# Install Homebrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Install Neovim and ripgrep
+brew install neovim ripgrep
+
+# Install packer to manage Neovim packages
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# To start nvim, install packer package and close it
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
