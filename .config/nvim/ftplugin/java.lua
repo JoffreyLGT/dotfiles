7dfc58c7adfd8780ef80a09dea98c322b3ffc1e5
Joffrey Lagut <j.lagut@live.fr>
2024-01-02 12:36:47 +0100

    feat(nvim): Setup Java development environment with jdtls

---
+++ .config/nvim/ftplugin/java.lua
@@ -0,0 +1,73 @@
+local home_dir = vim.fn.expand('$HOME')
+
+-- If you started neovim within `~/dev/xy/project-1` this would resolve to `project-1`
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+
+local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+
+    'java', -- or '/path/to/java17_or_newer/bin/java'
+    -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+    -- 💀
+    '-jar',
+    -- home_dir .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.600.v20231106-1826.jar',
+    vim.fn.glob(
+      home_dir ..
+      '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+
+    '-configuration', home_dir .. '/.local/share/nvim/mason/packages/jdtls/config_mac',
+
+    -- See `data directory configuration` section in the README
+    '-data', home_dir .. '/workspace/jdtls_projects/' .. project_name,
+  },
+
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  -- root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew' }),
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
+  settings = {
+    java = {
+    }
+  },
+
+  -- Language server `initializationOptions`
+  -- You need to extend the `bundles` with paths to jar files
+  -- if you want to use additional eclipse.jdt.ls plugins.
+  --
+  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+  --
+  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+  init_options = {
+    bundles = {
+      vim.fn.glob(
+        home_dir ..
+        "/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar", 1)
+    }
+  },
+
+  handlers = {
+    ['$/progress'] = function(_, result, ctx)
+      -- disable progress updates to avoid messages displayed for every character input
+    end,
+  },
+}
+vim.keymap.set('n', '<F4>', ":JdtUpdateDebugConfig<cr>", { desc = "Jdtls: Update debug config" })
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
+require('jdtls').start_or_attach(config)
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,42 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -369,7 +330,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
-    "astro" },
+    "astro", "java" },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,
@@ -505,11 +466,13 @@ local servers = {
           useLibraryCodeForTypes = true
         }
       }
-    }
+    },
   },
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  -- Java development using eclipse.jdt.ls
+  -- Configuration is done in ftplugin/java.lua
+  jdtls = {},
+
+  -- Python linter and formatter
   ruff_lsp = {
     settings = {
       -- Any extra CLI arguments for 'ruff'
@@ -539,7 +502,7 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
-mason_lspconfig.setup_handlers {
+local handlers = {
   function(server_name)
     require('lspconfig')[server_name].setup {
       init_options = (servers[server_name] or {}).init_options,
@@ -548,11 +511,17 @@ mason_lspconfig.setup_handlers {
       settings = servers[server_name],
       filetypes = (servers[server_name] or {}).filetypes,
     }
-  end
+  end,
+  -- Override the default configuration for jdtls to ensure mason-lspconfig doesn't
+  -- configure it. jdtls is configured in ftplugin/java.lua.
+  ["jdtls"] = function() end,
 }
 
--- Special case for efm
--- Out-of-box configuration available here: https://github.com/creativenull/efmls-configs-nvim
+mason_lspconfig.setup_handlers(handlers)
+
+-- FIXME is this special case still necessary? Could be setup with others.
+-- Special case for efm. Out-of-box configuration available here:
+-- https://github.com/creativenull/efmls-configs-nvim
 require("lspconfig").efm.setup {
   on_attach = on_attach,
   capabilities = capabilities,
